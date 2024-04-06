6ce7497b852b3eb20f0651e0cb8a270474472196
Aeroxee <vipercode305@gmail.com>
2024-04-06 22:35:32 +0700

    add cors

--- next.config.mjs
+++ next.config.mjs
@@ -39,6 +39,26 @@ const nextConfig = {
   //   locales: ["en", "id"],
   //   defaultLocale: "en",
   // },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token,X-Requested-With,Accept,Accept-Version,Content-Length,Content-MD5,Content-Type,Date,X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
